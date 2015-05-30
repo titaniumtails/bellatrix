f9142ae55fc2e8963f17b0e3be959e8fb4b6d65b
taliascotchbrook <taliaisontheweb@gmail.com>
2015-05-31 00:10:01 +0800

    Adjustment to file structure, comment out code

--- routes.js
+++ routes.js
@@ -1,13 +1,8 @@
-/**
- * Add your routes here
- */
+// /**
+//  * Add your routes here
+//  */
 
-Router.route('/', {
-  name: 'home',
-  layoutTemplate: 'layout'
-});
-
-Router.route('/posts/:_id', {
-  name: 'post',
-  layoutTemplate: 'layout'
-});
+// Router.route('/', {
+//   name: 'home',
+//   layoutTemplate: 'main'
+// });
